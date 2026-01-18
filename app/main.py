bf9f35dc8bf9b857489b030551041398647d7b6a
Shaydullin Ilfat <sweden.fan@yandex.ru>
2026-01-18 13:35:18 +0300

    refractoring & added asyncconextmanager

--- app/main.py
+++ app/main.py
@@ -1,24 +1,43 @@
-from fastapi import FastAPI
-import sys
+# app/main.py
 import os
-import uvicorn
-# Добавляем корень проекта в путь, чтобы можно было импортировать "app"
+import sys
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+# Убираем эту строку, если запускаем из корня проекта
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app.core.config import settings
+from app.core.db import Base, engine
 from app.modules.kts_dashboard.router import router as kts_router
 
 
-app = FastAPI(description='KTS Dashboard')
-from app.core.config import settings
-
-####### ВРЕМЕННЫЙ ПРИНТ #######
+# Проверка подключения к БД
 print("DB_URL:", settings.DB_URL)
-####### ВРЕМЕННЫЙ ПРИНТ #######
 
-# Регистрируем роутер для KTS Dashboard
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Запуск приложения...")
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    print("Таблицы созданы")
+    yield
+    await engine.dispose()
+    print("Приложение остановлено")
+
+
+# Создаём приложение после определения lifespan
+app = FastAPI(description='KTS Dashboard', lifespan=lifespan)
+
+
+# Подключаем роуты
 app.include_router(kts_router, prefix="/api")
 
 
+
 if __name__ == '__main__':
+    import uvicorn
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
