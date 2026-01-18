dd7189524fdd8ef54453a33c9b45e900bbebe726
Shaydullin Ilfat <sweden.fan@yandex.ru>
2026-01-18 13:35:37 +0300

    refractoring

--- app/core/db.py
+++ app/core/db.py
@@ -1,12 +1,32 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from typing import AsyncGenerator
+import os
 from core.config import settings
-from sqlalchemy.orm import DeclarativeBase
+
+DATABASE_URL = settings.DB_URL
 
 
-engine = create_async_engine(settings.DB_URL) # можно использовать echo=True для небольшого логирования
+# Создаём асинхронный движок
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+)
 
-async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+# Фабрика асинхронных сессий
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
 
 
-class Base(DeclarativeBase):
-    pass
+# Базовый класс для моделей
+Base = declarative_base()
+
+
+# Правильно типизированная зависимость
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
