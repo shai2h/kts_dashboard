04b7c4e037aa94449e419f35f903e66341c90c89
Ilfat Shaydullin <sweden.fan@yandex.ru>
2026-01-26 09:52:33 +0300

    added unique constraint

--- backend/app/modules/kts_dashboard/models.py
+++ backend/app/modules/kts_dashboard/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Numeric
 from app.core.db import Base
-
+from sqlalchemy import UniqueConstraint
 
 class KtsManagerPlan(Base):
     __tablename__ = "kts_manager_plan"
@@ -8,6 +8,11 @@ class KtsManagerPlan(Base):
     id = Column(Integer, primary_key=True)
     manager = Column(String, nullable=False)
     podr = Column(String, nullable=False)
-    plan = Column(Numeric(18, 2), nullable=False)
-    tec = Column(Numeric(18, 2), nullable=True)
-    procent = Column(Numeric(10, 2), nullable=False)
+
+    plan = Column(Numeric)
+    tec = Column(Numeric)
+    procent = Column(Numeric)
+
+    __table_args__ = (
+        UniqueConstraint("manager", "podr", name="uq_manager_podr"),
+    )
