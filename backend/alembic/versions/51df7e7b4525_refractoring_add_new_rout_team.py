e2a3bc283b5c8a18aef62e54cf07dc535426a525
Ilfat Shaydullin <sweden.fan@yandex.ru>
2026-02-17 15:46:17 +0300

    added team_row & created migrations

---
+++ backend/alembic/versions/51df7e7b4525_refractoring_add_new_rout_team.py
@@ -0,0 +1,24 @@
+"""refractoring & add new rout /team
+
+Revision ID: 51df7e7b4525
+Revises: 15114bef02f7
+Create Date: 2026-02-17 09:59:07.662050
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '51df7e7b4525'
+down_revision = '15114bef02f7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
